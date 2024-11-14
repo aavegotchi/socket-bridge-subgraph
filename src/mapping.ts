6545582b5a668f53fd94e51bbef7a99d7f6acbfb
Coderdan <dmathieuva@gmail.com>
2024-11-14 16:05:06 +0800

    chore: fix build error

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,8 +1,4 @@
 import { BridgingTokens, TokensBridged } from "../generated/ghst/Contract";
-import {
-  BridgingTokens as BridgingNFTs,
-  TokensBridged as NFTsBridged,
-} from "../generated/gotchi/NFTContract";
 import { TokenContract } from "../generated/schema";
 import { ChainId, TX_BRIDGED, TX_BRIDGING } from "./constants";
 import { findTokenForChain, getOrCreateBridgeTransfer } from "./helper";
@@ -77,67 +73,6 @@ export function handleTokensBridgedOnBaseSepolia(event: TokensBridged): void {
   }
 }
 
-//NFTs
-
-export function handleBridgingNFTsOnBaseSepolia(event: BridgingNFTs): void {
-  let tokenContract = findTokenForChain(event.address, ChainId.BASE_SEPOLIA);
-  if (tokenContract) {
-    handleBridgingNFTs(event, tokenContract);
-  }
-}
-
-export function handleNFTsBridgedOnBaseSepolia(event: NFTsBridged): void {
-  let tokenContract = findTokenForChain(event.address, ChainId.BASE_SEPOLIA);
-  if (tokenContract) {
-    handleNFTsBridged(event, tokenContract);
-  }
-}
-
-function handleBridgingNFTs(
-  event: BridgingNFTs,
-  tokenContract: TokenContract
-): void {
-  // TODO: Implement
-  const txHash = event.transaction.hash;
-  const id = txHash.toHex();
-  let entity = getOrCreateBridgeTransfer(id);
-  entity.messageId = event.params.messageId;
-  entity.tokenContract = tokenContract.id;
-  entity.connector = event.params.connector;
-  entity.sender = event.params.sender;
-  entity.receiver = event.params.receiver;
-  entity.amount = event.params.amount;
-  entity.fee = event.transaction.value;
-  entity.txHash = txHash;
-  entity.timestamp = event.block.timestamp;
-  entity.type = TX_BRIDGING;
-  entity.tokenId = event.params.tokenId;
-  entity.blockNumber = event.block.number;
-  entity.save();
-}
-
-function handleNFTsBridged(
-  event: NFTsBridged,
-  tokenContract: TokenContract
-): void {
-  // TODO: Implement
-
-  const txHash = event.transaction.hash;
-  const id = txHash.toHex();
-  let entity = getOrCreateBridgeTransfer(id);
-  entity.messageId = event.params.messageId;
-  entity.tokenContract = tokenContract.id;
-  entity.connector = event.params.connecter;
-  entity.receiver = event.params.receiver;
-  entity.amount = event.params.amount;
-  entity.txHash = txHash;
-  entity.timestamp = event.block.timestamp;
-  entity.type = TX_BRIDGED;
-  entity.tokenId = event.params.tokenId;
-  entity.blockNumber = event.block.number;
-  entity.save();
-}
-
 function handleBridgingTokens(
   event: BridgingTokens,
   tokenContract: TokenContract
@@ -156,6 +91,7 @@ function handleBridgingTokens(
   entity.txHash = txHash;
   entity.timestamp = event.block.timestamp;
   entity.type = TX_BRIDGING;
+  entity.blockNumber = event.block.number;
   entity.save();
 }
 
@@ -175,5 +111,6 @@ function handleTokensBridged(
   entity.txHash = txHash;
   entity.timestamp = event.block.timestamp;
   entity.type = TX_BRIDGED;
+  entity.blockNumber = event.block.number;
   entity.save();
 }
---
+++ src/mapping_nft.ts
@@ -0,0 +1,65 @@
+import { BridgingTokens, TokensBridged } from "../generated/gotchi/NFTContract";
+import { TokenContract } from "../generated/schema";
+import { ChainId, TX_BRIDGED, TX_BRIDGING } from "./constants";
+import { findTokenForChain, getOrCreateBridgeTransfer } from "./helper";
+
+//NFTs
+
+export function handleBridgingNFTsOnBaseSepolia(event: BridgingTokens): void {
+  let tokenContract = findTokenForChain(event.address, ChainId.BASE_SEPOLIA);
+  if (tokenContract) {
+    handleBridgingNFTs(event, tokenContract);
+  }
+}
+
+export function handleNFTsBridgedOnBaseSepolia(event: TokensBridged): void {
+  let tokenContract = findTokenForChain(event.address, ChainId.BASE_SEPOLIA);
+  if (tokenContract) {
+    handleNFTsBridged(event, tokenContract);
+  }
+}
+
+function handleBridgingNFTs(
+  event: BridgingTokens,
+  tokenContract: TokenContract
+): void {
+  // TODO: Implement
+  const txHash = event.transaction.hash;
+  const id = txHash.toHex();
+  let entity = getOrCreateBridgeTransfer(id);
+  entity.messageId = event.params.messageId;
+  entity.tokenContract = tokenContract.id;
+  entity.connector = event.params.connector;
+  entity.sender = event.params.sender;
+  entity.receiver = event.params.receiver;
+  entity.amount = event.params.amount;
+  entity.fee = event.transaction.value;
+  entity.txHash = txHash;
+  entity.timestamp = event.block.timestamp;
+  entity.type = TX_BRIDGING;
+  entity.tokenId = event.params.tokenId;
+  entity.blockNumber = event.block.number;
+  entity.save();
+}
+
+function handleNFTsBridged(
+  event: TokensBridged,
+  tokenContract: TokenContract
+): void {
+  // TODO: Implement
+
+  const txHash = event.transaction.hash;
+  const id = txHash.toHex();
+  let entity = getOrCreateBridgeTransfer(id);
+  entity.messageId = event.params.messageId;
+  entity.tokenContract = tokenContract.id;
+  entity.connector = event.params.connecter;
+  entity.receiver = event.params.receiver;
+  entity.amount = event.params.amount;
+  entity.txHash = txHash;
+  entity.timestamp = event.block.timestamp;
+  entity.type = TX_BRIDGED;
+  entity.tokenId = event.params.tokenId;
+  entity.blockNumber = event.block.number;
+  entity.save();
+}
